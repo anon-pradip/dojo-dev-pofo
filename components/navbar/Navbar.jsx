f91923a02805d65ac21795e756e3d67b1675e836
anon-pradip <pradipsubedi360@gmail.com>
2023-06-03 22:58:06 +0545

    change mobile nav

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -23,33 +23,22 @@ const navigation = [
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
   const pathname = usePathname();
-  const inactiveclass = " px-2 py-1 capitalize";
+  const inactiveclass =
+    " px-3 py-2 capitalize hover:bg-slate-400 hover:text-black hover:rounded-md";
   const activeclass =
     inactiveclass +
     " bg-slate-300 text-black rounded-md transition-all ease-out duration-700 ";
   return (
     <>
-      <div className=" w-full flex justify-between items-center py-5 ease-out">
-        <div>
-          <Link href={"/"} className={`${font.className} text-3xl`}>
-            Dojo-Dev
-          </Link>
-        </div>
-        <div className="hidden lg:flex justify-between items-center space-x-3 ">
-          {navigation.map((navItem) => (
-            <Link
-              href={navItem.href}
-              key={navItem.id}
-              className={
-                pathname === navItem.href ? activeclass : inactiveclass
-              }
-            >
-              {navItem.name}
+      <div className="w-full py-5">
+        <div className="flex w-full justify-between bg-red-500 grow">
+          <div>
+            <Link href={"/"} className={`${font.className} text-3xl`}>
+              Dojo-Dev
             </Link>
-          ))}
-        </div>
-        {isMobile && (
-          <div className="hidden  lg:flex justify-between items-center space-x-3 ">
+          </div>
+
+          <div className="hidden lg:flex justify-between space-x-3 ">
             {navigation.map((navItem) => (
               <Link
                 href={navItem.href}
@@ -62,17 +51,33 @@ const Navbar = () => {
               </Link>
             ))}
           </div>
-        )}
-      </div>
-
-      <div className=" block lg:hidden">
-        <button onClick={() => setIsMobile(!isMobile)}>
-          {isMobile ? (
-            <XCircleIcon height={12} width={12} />
-          ) : (
-            <Bars3Icon height={12} width={12} />
-          )}
-        </button>
+          <div className=" flex relative">
+            <div className=" block lg:hidden">
+              <button onClick={() => setIsMobile(!isMobile)}>
+                {isMobile ? (
+                  <XCircleIcon height={30} width={30} />
+                ) : (
+                  <Bars3Icon height={30} width={30} />
+                )}
+              </button>
+            </div>
+            {isMobile && (
+              <div className="lg:hidden flex flex-col bg-slate-800 rounded-md absolute right-8">
+                {navigation.map((navItem) => (
+                  <Link
+                    href={navItem.href}
+                    key={navItem.id}
+                    className={
+                      pathname === navItem.href ? activeclass : inactiveclass
+                    }
+                  >
+                    {navItem.name}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
