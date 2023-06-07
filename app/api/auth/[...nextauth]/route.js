1de65c8bd06cd4b5de897413ba626256a0be4fc3
anon-pradip <pradipsubedi360@gmail.com>
2023-06-07 21:10:39 +0545

    make credentialProvider for user login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,42 @@
+import User from "@/models/User";
+import connect from "@/utils/db";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
+
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET_KEY,
+    }),
+    CredentialsProviders({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connect();
+
+        try {
+          const user = await User.findOne({ email: credentials.email });
+
+          if (user) {
+            // check password
+            const isPassword = bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPassword) {
+              return user;
+            } else {
+              throw new Error("Wrong Credentials!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
     }),
   ],
 };
